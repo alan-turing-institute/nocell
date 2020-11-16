9eac1d080b9b907b7f576a2824a95ea68a8aa1c9
callummole <cmole@turing.ac.uk>
2020-11-16 12:04:47 +0000

    delete unwanted files

--- ods/ods-to-grid.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-(require sxml)
-
-(define NS_abbrev '((office . "urn:oasis:names:tc:opendocument:xmlns:office:1.0")
-                    (table . "urn:oasis:names:tc:opendocument:xmlns:table:1.0")))
-
-(define fn "flat_example.xml")
-(call-with-input-file fn
-    (lambda (in) (ssax:xml->sxml in NS_abbrev)))
