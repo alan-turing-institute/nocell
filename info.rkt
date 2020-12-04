1594d18043479dede1e03fc446580e71123c7029
Oliver Strickson <ostrickson@turing.ac.uk>
2020-12-04 16:00:01 +0000

    Fix dependencies in info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "nocell")
-(define deps '("base" "rackunit-lib" "sxml" "gregor" "http://github.com/alan-turing-institute/whatnow.git"))
+(define deps '("base" "rackunit-lib" "sxml" "gregor" "https://github.com/alan-turing-institute/whatnow.git"))
 (define pkg-desc "A language for building probabilistic spreadsheets")
 (define version "0.1")
 (define pkg-authors '(callummole ots22 tamc triangle-man))
