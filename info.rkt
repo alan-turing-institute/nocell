d2acd38732a7c3f8bee6f07ba93cc0332149d5dd
Oliver Strickson <ostrickson@turing.ac.uk>
2021-07-07 13:46:28 +0100

    Pin whatnow version

--- info.rkt
+++ info.rkt
@@ -4,7 +4,7 @@
                "rackunit-lib"
                "sxml"
                "gregor"
-               "https://github.com/alan-turing-institute/whatnow.git#main"
+               "https://github.com/alan-turing-institute/whatnow.git#62055d008faea5fe3c5779804b986c6a3d97873d"
                "basedir" ; for whatnow
                ))
 (define pkg-desc "A language for building probabilistic spreadsheets")
