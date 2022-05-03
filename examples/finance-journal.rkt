5f38f905affa632527b920102905e84ac2960820
Oliver Strickson <ostrickson@turing.ac.uk>
2022-05-03 17:41:34 +0100

    WIP Add separate finance reporting example, formatted as 'journal' entries

---
+++ examples/finance-journal.rkt
@@ -0,0 +1,52 @@
+#lang racket
+
+;; A racket spreadsheet that emits the team's monthly project-person
+;; allocation report for Finance.
+
+;; https://github.com/alan-turing-institute/nocell/issues/49
+
+;; This example could eventually move into its own repository, with
+;; nocell and whatnow as dependencies
+
+
+(require (rename-in gregor
+                    [date gregor:date]
+                    [date? gregor:date?])
+         whatnow/forecast
+         whatnow/schedule
+         racket/hash
+         rackunit
+         "../sheet/sheet.rkt"
+         "../sheet/ods/ods.rkt"
+         "finance-utils.rkt")
+
+
+;; (bytes->file (make-report-ods example-allocations (fy-months 2020))
+;;              "example-report-to-finance.fods")
+
+(let* ([months (map (curry +months (gregor:date 2022 04)) (range 1))]
+
+       [allocations
+        (for/fold ([allocations null])
+                  ([month (in-list months)])
+          (let* ([iso-month-end (-days (iso-week-start (+months month 1)) 1)]
+                 [forecast-schedule (get-the-schedule (iso-week-start month) iso-month-end)])
+            (append allocations
+                    (schedule->allocations forecast-schedule (list month)))))]
+
+       [allocations-grouped
+        (group-by (λ (a) (list (allocation-client a)
+                               (allocation-person a)
+                               (allocation-project a)))
+                  allocations)]
+
+       [allocations-combined
+        (map (λ (as)
+               (struct-copy allocation
+                            (car as)
+                            [month-fraction
+                             (apply hash-union (map allocation-month-fraction as))]))
+             allocations-grouped)])
+
+  (bytes->file (make-report-ods allocations-combined months)
+               "finance-journal.ods"))
